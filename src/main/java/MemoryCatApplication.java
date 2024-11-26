e85c5cd1f0b9a8c24432ed3155e917cd215c6e42
kimD0ngjun <chickentasty0112@gmail.com>
2024-11-26 18:49:42 +0900

    fix: out 패키지 생성 및 터미널 실행 방향 세팅

--- src/main/java/MemoryCatApplication.java
+++ src/main/java/MemoryCatApplication.java
@@ -1,5 +1,9 @@
 import controller.MemoryCatController;
 
+/**
+ * out 패키지 생성 : java -cp out/production/classes MemoryCatApplication
+ * 실행 : java -cp out/production/classes MemoryCatApplication
+ */
 public class MemoryCatApplication {
     public static void main(String[] args) {
         MemoryCatController memoryCatController = new MemoryCatController();
