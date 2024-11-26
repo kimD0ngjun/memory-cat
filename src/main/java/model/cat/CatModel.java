689af9e55d61dd3a7997f7066a1331dbca3b1338
kimD0ngjun <chickentasty0112@gmail.com>
2024-11-26 18:14:25 +0900

    feat: 고양이 문자열 프레임 모델 기본 생성

---
+++ src/main/java/model/cat/CatModel.java
@@ -0,0 +1,26 @@
+package model.cat;
+
+public class CatModel {
+    public static final String[] SLEEPING_CAT_FRAMES = {
+            "고양이 위로 나비가 날아간다냥\n\n" +
+                    "       εїз\n\n" +
+                    "   ∧  ヘ\n" +
+                    "  (  ̮  ̮  )\n" +
+                    "   |_  <) \n" +
+                    "  ()し(~)〜〜\n",
+            "고양이 위로 나비가 날아간다냥\n\n" +
+                    "    εїз...\n\n" +
+                    "   ∧  ヘ\n" +
+                    "  (。  ̮  )\n" +
+                    "  ∠    <) \n" +
+                    "  ()し(~)〜〜\n",
+            "고양이 위로 나비가 날아간다냥\n\n" +
+                    "  εїз.....\n\n" +
+                    "   ﾉヽ\n" +
+                    "  (˚. 。 フ\n" +
+                    "  ∠    >)  \n" +
+                    "  ()し(~)〜〜\n"
+    };
+
+    private static final String[] WARNING_CAT_FRAMES = {};
+}
