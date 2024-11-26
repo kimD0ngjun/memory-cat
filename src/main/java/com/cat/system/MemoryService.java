c2d804aebb786715b2fefbc009e4d7233251ef88
kimD0ngjun <chickentasty0112@gmail.com>
2024-11-26 22:40:21 +0900

    feat: 헤더 파일 및 디컴파일드 파일 생성

--- src/main/java/com/cat/system/MemoryService.java
+++ src/main/java/com/cat/system/MemoryService.java
@@ -5,7 +5,10 @@ public class MemoryService {
     public native int getUsedMemory();
 
     static {
-        System.loadLibrary("native-lib");
+        try {
+            System.loadLibrary("native-lib");
+        } catch (UnsatisfiedLinkError e) {
+            throw new RuntimeException("네이티브 라이브러리 실행 실패: native-lib", e);
+        }
     }
-
 }
