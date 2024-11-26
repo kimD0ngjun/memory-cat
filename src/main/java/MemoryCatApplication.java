7eee0f36bc20d3ec12f757d81a509afd1aa934a8
kimD0ngjun <chickentasty0112@gmail.com>
2024-11-26 18:21:08 +0900

    feat: 메인 스레드 할당

---
+++ src/main/java/MemoryCatApplication.java
@@ -0,0 +1,8 @@
+import controller.MemoryCatController;
+
+public class MemoryCatApplication {
+    public static void main(String[] args) {
+        MemoryCatController memoryCatController = new MemoryCatController();
+        memoryCatController.runAnimation();
+    }
+}
