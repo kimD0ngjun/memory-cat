00f6ec70d7f945afac57a0a181140fbf14da832f
kimD0ngjun <chickentasty0112@gmail.com>
2024-11-26 18:16:49 +0900

    feat: 고양이 문자열 프레임 뷰 작성

---
+++ src/main/java/view/CatView.java
@@ -0,0 +1,19 @@
+package view;
+
+public class CatView {
+
+    public void displayFrame(String frame) {
+        System.out.print(frame);
+    }
+
+    // 이전 프레임 지우기 (콘솔 화면 제어)
+    public void clearLines(int lineCount) {
+        StringBuilder clearCommand = new StringBuilder();
+        for (int i = 0; i < lineCount; i++) {
+            clearCommand.append("\033[F"); // 커서를 한 줄 위로 이동
+            clearCommand.append("\033[2K"); // 현재 줄을 지움
+        }
+        System.out.print(clearCommand);
+    }
+
+}
