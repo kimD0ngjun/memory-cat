44fb9007b787c388e9a2a9ff293e9f41c2d23b9f
kimD0ngjun <chickentasty0112@gmail.com>
2024-11-26 19:06:23 +0900

    fix: out 패키지 생성 및 터미널 실행 방향 세팅

--- src/main/java/MemoryCatApplication.java
+++ src/main/java/MemoryCatApplication.java
@@ -1,6 +1,7 @@
 import controller.MemoryCatController;
 
 /**
+ * 얘네는 개발환경에서만 쓰는 것, 실제 배포 파일은 jar 파일로 배포
  * out 패키지 생성 : java -cp out/production/classes MemoryCatApplication
  * 실행 : java -cp out/production/classes MemoryCatApplication
  */
